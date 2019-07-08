7ea3a275264d493a23f60fb77c099b11fbe52980
hyecheon <rainbow880616@gmail.com>
2019-07-09 01:05:35 +0900

    Login Functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import store from "./store";
 import AddClient from "./components/clients/AddClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import EditClient from "./components/clients/EditClient";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
               <Route exact path="/client/add" component={AddClient}/>
               <Route exact path="/client/edit/:id" component={EditClient}/>
               <Route exact path="/client/:id" component={ClientDetails}/>
+              <Route exact path="/login" component={Login}/>
             </Switch>
           </div>
         </div>
--- src/store.js
+++ src/store.js
@@ -9,7 +9,8 @@ import {firebaseConfig} from "./firebaseConfig";
 // react-redux-firebase config
 const rrfConfig = {
   userProfile: "users",
-  userFirestoreForProfile: true,
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+
 };
 
 //Init firebase instance
